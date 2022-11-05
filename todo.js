0961c4415d6716b76b0d711242badfab44399193
ConnorMcGehee <connorjames050@gmail.com>
2022-11-05 01:13:07 -0500

    Added filtering

--- todo.js
+++ todo.js
@@ -25,11 +25,78 @@ const logTodos = () => {
 }
 
 const populateTodos = () => {
-    let ul = document.getElementById("todo-list");
+    let ol = document.getElementById("todo-list");
+    ol.innerHTML = "";
+    let comListEl = document.getElementById("completed-list");
+    comListEl.innerHTML = "";
+    let incListEl = document.getElementById("incomplete-list");
+    incListEl.innerHTML = "";
+
     for (let item of arrayOfTodos) {
         let li = document.createElement("li")
         li.innerHTML = item.title;
-        
-        ul.appendChild(li);
+
+        ol.appendChild(li);
+    }
+
+    let filteredCompleted = arrayOfTodos.filter((todoItem) => todoItem.completed);
+    for (let item of filteredCompleted) {
+
+        let li = document.createElement("li")
+        li.innerHTML = item.title;
+
+        comListEl.appendChild(li);
+    }
+
+    let filteredIncomplete = arrayOfTodos.filter((todoItem) => !todoItem.completed);
+    for (let item of filteredIncomplete) {
+
+        let li = document.createElement("li")
+        li.innerHTML = item.title;
+
+        incListEl.appendChild(li);
+    }
+}
+
+let filteredArray = [];
+const filterTodosUserId = () => {
+    let ol = document.getElementById("todo-list");
+    ol.innerHTML = "";
+
+    let userId = document.getElementById("userId").value;
+
+    filteredArray = arrayOfTodos.filter((todoItem) => todoItem.userId === parseInt(userId));
+    for (let item of filteredArray) {
+
+        let li = document.createElement("li")
+        li.innerHTML = item.title;
+
+        ol.appendChild(li);
+    }
+
+    let comListEl = document.getElementById("completed-list");
+    comListEl.innerHTML = "";
+
+    filteredArray = arrayOfTodos.filter((todoItem) => todoItem.userId === parseInt(userId));
+    let filteredCompleted = filteredArray.filter((todoItem) => todoItem.completed);
+    for (let item of filteredCompleted) {
+
+        let li = document.createElement("li")
+        li.innerHTML = item.title;
+
+        comListEl.appendChild(li);
+    }
+
+    let incListEl = document.getElementById("incomplete-list");
+    incListEl.innerHTML = "";
+
+    filteredArray = arrayOfTodos.filter((todoItem) => todoItem.userId === parseInt(userId));
+    let filteredIncomplete = filteredArray.filter((todoItem) => !todoItem.completed);
+    for (let item of filteredIncomplete) {
+
+        let li = document.createElement("li")
+        li.innerHTML = item.title;
+
+        incListEl.appendChild(li);
     }
 }
